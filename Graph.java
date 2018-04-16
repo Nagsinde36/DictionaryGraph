e30575ca5f4a4f0cd81b216441b0a0c0d2f6c9e0
shirleyshao <37048995+shirleyshao@users.noreply.github.com>
2018-04-16 16:27:54 -0500

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -17,54 +17,47 @@
 //
 // 2018 Apr 16, 2018 WordProcessor.java 
 ////////////////////////////80 columns wide //////////////////////////////////
-import java.awt.Color;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
-import com.jogamp.graph.geom.Vertex;
+
 
 /**
  * Undirected and unweighted graph implementation
  * 
  * @param <E> type of a vertex
  * 
- * @author sapan (sapan@cs.wisc.edu)
+ * @author  (sapan@cs.wisc.edu)
  * 
  */
 public class Graph<E> implements GraphADT<E> {
     
-    final int MAX_VERTEX = 20;// 最多20个顶点  
-    E[] vertices;// 顶点数组  
-    int[][] adjacency;// 邻接矩阵  
-    int numOfVertex;// 当前图中顶点的数量  
-    /*public class Vertex<T>{
-        public E label;
-        public Boolean wasVisited;
-        public Vertex(E lab){
-            label = lab;
-            wasVisited = false;
-        }
-    }*/
-    public Graph() {// 构造器  
-        this.vertices = (E[]) new Object[MAX_VERTEX];  
+    int MAX_VERTEX = 20;// maximum capacity of vertex  
+    E[] vertices;// vertices  
+    int[][] adjacency;// adjacent matrix  
+    int numOfVertex;// the number of vertexes  
+    /**
+     * Instance variables and constructors
+     */
+    public Graph() {  
+        vertices = (E[]) new Object[MAX_VERTEX];  
         adjacency = new int[MAX_VERTEX][MAX_VERTEX];  
         numOfVertex = 0;  
-        // 将邻接矩阵初始化  
+        //initial the adjacent matrix
         for (int i = 0; i < MAX_VERTEX; i++) {  
             for (int j = 0; j < MAX_VERTEX; j++)  
-                adjacency[i][j] = 0;  
+                adjacency[i][j] = 0; // represent no edge between two vertxes 
         }  
     }
     /**
-     * Instance variables and constructors
+     * Expand the capacity of adjacent matrix and vertices array
+     * 
+     * Help method
      */
     private void expandCapacity()
     {
         E[] largerVertices = (E[])(new Object[vertices.length*2]);
-       int [][] largerAdjacency = 
-             new int[vertices.length*2][vertices.length*2];
+       int [][] largerAdjacency = new int[vertices.length*2][vertices.length*2];
 
        for (int i = 0; i < this.numOfVertex; i++)
        {
@@ -78,20 +71,46 @@ public class Graph<E> implements GraphADT<E> {
        this.vertices = largerVertices;
        this.adjacency = largerAdjacency;
     }
-    public int getIndex(E vertex)
+    /**
+     * Get the index of vertex in the vertices array
+     * 
+     * Help method
+     * 
+     * @return the index if vertex in the graph, else return -1
+     */
+    private int getIndex(E vertex)
     {
        for (int i = 0; i < numOfVertex; i++)
           if (vertices[i].equals(vertex))
              return i;
        return -1;
     }
+    /**
+     * Check whether the vertex in the graph
+     * 
+     * Help method
+     * 
+     * @return true if vertex in the graph, else return false
+     */
     private Boolean containVertex(E vertex) {
         for(int i=0; i< this.numOfVertex;i++) {
-            if(this.vertices[i] == vertex)
+            if(this.vertices[i].equals(vertex))
                 return true;
           }
         return false;
     }
+    
+    /**
+     * Add new vertex to the graph
+     * 
+     * Valid argument conditions:
+     * 1. vertex should be non-null
+     * 2. vertex should not already exist in the graph 
+     * 
+     * @param vertex the vertex to be added
+     * @return vertex if vertex added, else return null if vertex can not be added (also if valid conditions are violated)
+     */
+    @Override
     public E addVertex(E vertex) {
         if(vertex == null)
             return null;
@@ -111,12 +130,18 @@ public class Graph<E> implements GraphADT<E> {
          numOfVertex++;
          return vertex;
     }
+    /**
+     * Remove the vertex in the graph by given index
+     * 
+     * Help method
+     * 
+     */
     private void removeVertex (int index)
     {
        
           numOfVertex--;
 
-          for (int i = index; i < numOfVertex; i++)
+          for (int i = index; i < numOfVertex; i++) //move the vertex location 
              vertices[i] = vertices[i+1];
 
           for (int i = index; i < numOfVertex; i++) {
@@ -132,19 +157,27 @@ public class Graph<E> implements GraphADT<E> {
     }
     
     /**
-     * {@inheritDoc}
+     * Remove the vertex and associated edge associations from the graph
+     * 
+     * Valid argument conditions:
+     * 1. vertex should be non-null
+     * 2. vertex should exist in the graph 
+     *  
+     * @param vertex the vertex to be removed
+     * @return vertex if vertex removed, else return null if vertex and associated edges can not be removed (also if valid conditions are violated)
      */
     @Override
     public E removeVertex(E vertex) {
-        for (int i = 0; i < numOfVertex; i++)
-        {
-           if (vertex ==vertices[i])
-           {
-              removeVertex(i);
+        if(vertex == null)
+            return null;
+        if(containVertex(vertex)== false)
+            return null;
+        
+           
+              removeVertex(getIndex(vertex));
               return vertex;
-           }
-        }
-        return null;
+           
+        
     }
     
     /*private void addEdgeHelper (int start, int end) {
@@ -154,7 +187,15 @@ public class Graph<E> implements GraphADT<E> {
         
     }*/
     /**
-     * {@inheritDoc}
+     * Add an edge between two vertices (edge is undirected and unweighted)
+     * 
+     * Valid argument conditions:
+     * 1. both the vertices should exist in the graph
+     * 2. vertex1 should not equal vertex2
+     *  
+     * @param vertex1 the first vertex
+     * @param vertex2 the second vertex
+     * @return true if edge added, else return false if edge can not be added (also if valid conditions are violated)
      */
     @Override
     
@@ -173,7 +214,15 @@ public class Graph<E> implements GraphADT<E> {
     
     
     /**
-     * {@inheritDoc}
+     * Remove the edge between two vertices (edge is undirected and unweighted)
+     * 
+     * Valid argument conditions:
+     * 1. both the vertices should exist in the graph
+     * 2. vertex1 should not equal vertex2
+     *  
+     * @param vertex1 the first vertex
+     * @param vertex2 the second vertex
+     * @return true if edge removed, else return false if edge can not be removed (also if valid conditions are violated)
      */
     @Override
     public boolean removeEdge(E vertex1, E vertex2) {
@@ -190,7 +239,15 @@ public class Graph<E> implements GraphADT<E> {
     }
 
     /**
-     * {@inheritDoc}
+     * Check whether the two vertices are adjacent
+     * 
+     * Valid argument conditions:
+     * 1. both the vertices should exist in the graph
+     * 2. vertex1 should not equal vertex2
+     *  
+     * @param vertex1 the first vertex
+     * @param vertex2 the second vertex
+     * @return true if both the vertices have an edge with each other, else return false if vertex1 and vertex2 are not connected (also if valid conditions are violated)
      */
     @Override
     public boolean isAdjacent(E vertex1, E vertex2) {
@@ -208,7 +265,14 @@ public class Graph<E> implements GraphADT<E> {
     }
 
     /**
-     * {@inheritDoc}
+     * Get all the neighbor vertices of a vertex
+     * 
+     * Valid argument conditions:
+     * 1. vertex is not null
+     * 2. vertex exists
+     * 
+     * @param vertex the vertex
+     * @return an iterable for all the immediate connected neighbor vertices
      */
     @Override
     public Iterable<E> getNeighbors(E vertex) {
@@ -225,7 +289,9 @@ public class Graph<E> implements GraphADT<E> {
     }
 
     /**
-     * {@inheritDoc}
+     * Get all the vertices in the graph
+     * 
+     * @return an iterable for all the vertices
      */
     @Override
     public Iterable<E> getAllVertices() {
@@ -237,4 +303,13 @@ public class Graph<E> implements GraphADT<E> {
         return setOfVertices;
     }
 
-}
+
+    
+             
+    
+         
+                
+          
+     
+        
+           
